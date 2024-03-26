db8018fd04b200a3726008e71f870952634d598e
Harsh Vyapari <harshv5094@gmail.com>
2024-03-26 14:53:56 +0000

    feat:Initial Commit

---
+++ .bash_aliases
@@ -0,0 +1,2 @@
+alias bash_alias_config="code ~/.bash_aliases"
+alias g="git"
---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -f "$HOME/.bash_aliases" ]; then
+  rm .bash_aliases 
+  cp .bash_aliases "$HOME"
+else
+  cp .bash_aliases "$HOME"
+fi
+
+if [ -f "$HOME/.gitconfig" ];then
+  rm .bash_aliases 
+  cp .bash_aliases "$HOME"
+else
+  cp .bash_aliases "$HOME"
+fi
+
+if command -v npm &> /dev/null; then
+  npm i -g commitizen cz-conventional-changelog
+fi
